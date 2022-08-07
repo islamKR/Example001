903243312b69cac9698596c0606a574a00b4d33f
islam <islamkrus17@gmail.com>
2022-08-07 14:55:10 +0400

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 10;
+int c = 19;
+int d = 12;
+int e = 23;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
