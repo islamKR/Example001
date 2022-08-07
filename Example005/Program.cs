41abd813f76c5629ebedc1bf11411f9a51ae9ef5
islam <islamkrus17@gmail.com>
2022-08-07 14:37:55 +0400

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your name");
+string UserName = Console.ReadLine();
+
+if(UserName.ToLower == "masha")
+{
+   Console.WriteLine("Wow? thats a Masha, i am glad to see you");
+}
+else
+{ 
+    Console.Write("hi stranger," );
+    Console.WriteLine(UserName);
+}
