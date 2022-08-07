5f7d9b774ef67a832777d79192bc129ba278674f
islam <islamkrus17@gmail.com>
2022-08-07 13:50:40 +0400

    somethings in git ignore

---
+++ Example002_Hello_user/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" Write your name");
+
+string UserName = Console.ReadLine();
+
+Console.WriteLine("Hello, ");
+
+Console.WriteLine(UserName);
